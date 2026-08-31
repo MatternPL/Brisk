f09eab1f981f2d37c432c276a8f10542cddbd9ba
Mathias <mathiaspilotarne@gmail.com>
2026-08-31 20:30:27 +0200

    Version 1.6.2

--- installer/Installer.cs
+++ installer/Installer.cs
@@ -13,7 +13,7 @@ namespace BriskSetup
     static class Setup
     {
         public const string AppName = "Brisk";
-        public const string Version = "1.6.1";
+        public const string Version = "1.6.2";
         public const string RegKey = @"Software\Microsoft\Windows\CurrentVersion\Uninstall\Brisk";
 
         public static string InstallDir
