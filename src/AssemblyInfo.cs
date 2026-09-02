03604615e88b9ab7243df167b912d711b60cdbf9
Mathias <mathiaspilotarne@gmail.com>
2026-09-02 19:13:31 +0200

    Brisk 1.7.0
    Forste signerte utgivelse. Verifisert av Windows selv: signaturen er OK,
    sha256, tidsstemplet av Certum, hele kjeden bakt inn i fila.
    
    Ellers: farge per skjerm, G-SYNC-visning der skjermen oppgir det,
    symbolfil med i installasjonen, og utgiver satt til navnet mitt.

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Brisk")]
 [assembly: AssemblyCompany("Mathias Arne Andresen")]
 [assembly: AssemblyCopyright("© 2026 Mathias Arne Andresen")]
-[assembly: AssemblyVersion("1.6.6.0")]
-[assembly: AssemblyFileVersion("1.6.6.0")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
 [assembly: ComVisible(false)]
